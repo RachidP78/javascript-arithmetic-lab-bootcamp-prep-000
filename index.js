53212a2814f498e97d016e01222ddedef5616d4e
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-20 06:22:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+var x
+var y
+
+
+function add(x, y) {
+  number = x + y
+}
+
+function subtract() {
+  
+}
+
+function multiply() {
+  
+}
+
+function divide() {
+  
+}
+
+function increment() {
+  
+}
+
+function decrement() {
+  
+}
+
+function makeInt() {
+  
+}
+
+function preserveDecimal() {
+  
+}
